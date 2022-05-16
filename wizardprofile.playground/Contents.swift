0af99ef640cb5eb275b01e2c078f67ee94eb1ea2
cdmccune <curtmccune@gmail.com>
2022-05-16 11:26:08 -0700

    completed lab

---
+++ wizardprofile.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let firstName = "Ron"
+let lastName = "Weasley"
+let birthYear = 1980
+let nationality = "English"
+let bloodStatus = "Pure Blood"
+let gender = "Male"
+var height = 1.75
+var weight = 69
+let isInHP = true
+let isInPrequel = false
+let wasInDumbArmy = true
+let wasDeathEater = false
+
+let howCrucial = 9
+let actorPerformance = 7
+let reasoning = "His humor is very quality. Thought him and Harry had their bumps at different points, in the end of the day he was very loyal and stood up when he needed to."
